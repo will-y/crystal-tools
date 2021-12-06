5a7e88e4df5859ec645fc5bda0f9144d14737fa9
Will Yelton <willhyelton@gmail.com>
2021-12-05 23:02:11 -0500

    Added a test button to the menu

--- src/main/java/dev/willyelton/crystal_tools/gui/UpgradeScreen.java
+++ src/main/java/dev/willyelton/crystal_tools/gui/UpgradeScreen.java
@@ -1,18 +1,36 @@
 package dev.willyelton.crystal_tools.gui;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 
 public class UpgradeScreen extends Screen {
     public UpgradeScreen() {
-        super(new TextComponent(""));
+        super(new TextComponent("Test Title"));
+
+
+    }
+
+    // FOR SOME REASON NEED TO ADD COMPONENTS HERE FOR SOME REASON????
+    protected void init() {
+        this.addRenderableWidget(new Button(this.width / 2 + 5, this.height / 6 + 72 - 6, 150, 20, new TextComponent("Test Button"), (button) -> {
+            System.out.println("Pressed Button");
+        }));
+
     }
 
     @Override
     public void render(PoseStack poseStack, int mouseX, int mouseY, float particleTicks) {
+        this.renderBackground(poseStack);
         super.render(poseStack, mouseX, mouseY, particleTicks);
-        fill(poseStack, 0, 0, 100, 100, 16777216);
+//        int i = (this.width - 252) / 2;
+//        int j = (this.height - 140) / 2;
+
+//        fill(poseStack, 0, 0, 100, 100, 16777216);
+//        this.blit(poseStack, 100, 100, 0, 0, 252, 140);
+
     }
+
+
 }
