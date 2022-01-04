520355d8d9c5f162913ad5eef36f05b11d3fd65f
Will Yelton <willhyelton@gmail.com>
2022-01-04 18:44:24 -0500

    Added a new keybind for vein mining

--- src/main/java/dev/willyelton/crystal_tools/keybinding/KeyBindings.java
+++ src/main/java/dev/willyelton/crystal_tools/keybinding/KeyBindings.java
@@ -7,9 +7,11 @@ import org.lwjgl.glfw.GLFW;
 
 public class KeyBindings {
     public static KeyMapping upgradeMenu;
+    public static KeyMapping veinMine;
 
     public static void init() {
         upgradeMenu = createBinding("upgrade_menu", GLFW.GLFW_KEY_K);
+        veinMine = createBinding("vein_mine", GLFW.GLFW_KEY_GRAVE_ACCENT);
     }
 
     private static KeyMapping createBinding(String name, int key) {
