2a95cf0ac4f800b1324fb8050c9e6d9003a7fe0f
Will Yelton <willhyelton@gmail.com>
2022-01-15 23:29:39 -0500

    Added exp when hurting enemy

--- src/main/java/dev/willyelton/crystal_tools/tool/SwordLevelableTool.java
+++ src/main/java/dev/willyelton/crystal_tools/tool/SwordLevelableTool.java
@@ -19,14 +19,24 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 
 public class SwordLevelableTool extends LevelableTool {
+    private final Multimap<Attribute, AttributeModifier> defaultModifiers;
+
     public SwordLevelableTool() {
         // Need an empty block tag
         super(new Item.Properties().fireResistant().tab(CreativeTabs.CRYSTAL_TOOLS_TAB), BlockTags.CARPETS, "sword");
+        ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_UUID, "Weapon modifier", (double)this.getDamage(), AttributeModifier.Operation.ADDITION));
+        builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_UUID, "Weapon modifier", (double)this.getAttackSpeed(), AttributeModifier.Operation.ADDITION));
+        this.defaultModifiers = builder.build();
     }
 
 
     public float getDamage() {
         return tier.getAttackDamageBonus();
+    }
+
+    public float getAttackSpeed() {
+        return 1;
     }
 
     public boolean canAttackBlock(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer) {
@@ -46,10 +56,12 @@ public class SwordLevelableTool extends LevelableTool {
         return pBlock.is(Blocks.COBWEB);
     }
 
-    public boolean hurtEnemy(ItemStack pStack, LivingEntity pTarget, LivingEntity pAttacker) {
-        pStack.hurtAndBreak(1, pAttacker, (p_43296_) -> {
+    public boolean hurtEnemy(ItemStack tool, LivingEntity target, LivingEntity attacker) {
+        tool.hurtAndBreak(1, attacker, (p_43296_) -> {
             p_43296_.broadcastBreakEvent(EquipmentSlot.MAINHAND);
         });
+
+        addExp(tool, target.getLevel(), attacker.getOnPos());
         return true;
     }
 
@@ -58,10 +70,11 @@ public class SwordLevelableTool extends LevelableTool {
 //        return pEquipmentSlot == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getDefaultAttributeModifiers(pEquipmentSlot);
 //    }
 
-//    @Override
-//    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
-//        return slot == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getAttributeModifiers(slot, stack);
-//    }
+    @Override
+    public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlot slot, ItemStack stack) {
+
+        return slot == EquipmentSlot.MAINHAND ? this.defaultModifiers : super.getAttributeModifiers(slot, stack);
+    }
 
     @Override
     public boolean canPerformAction(ItemStack stack, net.minecraftforge.common.ToolAction toolAction) {
