8b4503ce072bae277d8990a0e0c0b4f46c798ae5
Will Yelton <willhyelton@gmail.com>
2022-01-27 12:06:40 -0500

    Armor now gains exp

---
+++ src/main/java/dev/willyelton/crystal_tools/event/LivingHurtEvent.java
@@ -0,0 +1,27 @@
+package dev.willyelton.crystal_tools.event;
+
+import dev.willyelton.crystal_tools.CrystalTools;
+import dev.willyelton.crystal_tools.item.armor.LevelableArmor;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid=CrystalTools.MODID)
+public class LivingHurtEvent {
+    @SubscribeEvent
+    public static void handleLivingHurtEvent(net.minecraftforge.event.entity.living.LivingHurtEvent event) {
+        LivingEntity entity = event.getEntityLiving();
+
+        if (entity instanceof Player player) {
+            float damageAmount = event.getAmount();
+
+            for (ItemStack armor : player.getArmorSlots()) {
+                if (armor.getItem() instanceof LevelableArmor item) {
+                    item.addExp(armor, player.getLevel(), player.getOnPos(), (int) damageAmount * 2);
+                }
+            }
+        }
+    }
+}
