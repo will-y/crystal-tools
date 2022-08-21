dcd38e94a5227fe3db63540ad1e2f51d9b17c24c
Will Yelton <willhyelton@gmail.com>
2022-08-21 12:41:29 -0400

    Refactors hoe

--- src/main/java/dev/willyelton/crystal_tools/item/tool/HoeLevelableTool.java
+++ src/main/java/dev/willyelton/crystal_tools/item/tool/HoeLevelableTool.java
@@ -3,22 +3,16 @@ package dev.willyelton.crystal_tools.item.tool;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.mojang.datafixers.util.Pair;
-import dev.willyelton.crystal_tools.utils.NBTUtils;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
+import dev.willyelton.crystal_tools.utils.ToolUseUtils;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
 import java.util.function.Consumer;
@@ -37,50 +31,8 @@ public class HoeLevelableTool extends LevelableTool {
         super(new Item.Properties(), BlockTags.MINEABLE_WITH_HOE, "hoe");
     }
 
-    // also just took this from HoeItem
-    // don't think I need to change other than give exp
     @Override
-    public InteractionResult useOn(UseOnContext context) {
-        ItemStack hoe = context.getItemInHand();
-        Level level = context.getLevel();
-        BlockPos blockpos = context.getClickedPos();
-
-        int durability = this.getMaxDamage(hoe) - (int) NBTUtils.getFloatOrAddKey(hoe, "Damage");
-
-        if (durability <= 1) {
-            return InteractionResult.PASS;
-        }
-
-        Pair<Predicate<UseOnContext>, Consumer<UseOnContext>> pair = TILLABLES.get(level.getBlockState(blockpos).getBlock());
-//        int hook = net.minecraftforge.event.ForgeEventFactory.onHoeUse(context);
-//        if (hook != 0) return hook > 0 ? InteractionResult.SUCCESS : InteractionResult.FAIL;
-        if (context.getClickedFace() != Direction.DOWN && level.isEmptyBlock(blockpos.above())) {
-            if (pair == null) {
-                return InteractionResult.PASS;
-            } else {
-                Predicate<UseOnContext> predicate = pair.getFirst();
-                Consumer<UseOnContext> consumer = pair.getSecond();
-                if (predicate.test(context)) {
-                    Player player = context.getPlayer();
-                    level.playSound(player, blockpos, SoundEvents.HOE_TILL, SoundSource.BLOCKS, 1.0F, 1.0F);
-                    if (!level.isClientSide) {
-                        consumer.accept(context);
-                        if (player != null) {
-                            context.getItemInHand().hurtAndBreak(1, player, (p_150845_) -> {
-                                p_150845_.broadcastBreakEvent(context.getHand());
-                            });
-                        }
-                    }
-
-                    addExp(hoe, level, blockpos, context.getPlayer());
-
-                    return InteractionResult.sidedSuccess(level.isClientSide);
-                } else {
-                    return InteractionResult.PASS;
-                }
-            }
-        }
-
-        return InteractionResult.PASS;
+    public @NotNull InteractionResult useOn(@NotNull UseOnContext context) {
+        return ToolUseUtils.useOnHoe(context, this);
     }
 }
