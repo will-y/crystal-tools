58d269036f4b9005fce44e3666437f679254937d
Will Yelton <willhyelton@gmail.com>
2022-08-24 14:25:14 -0400

    Recipe for elytra now uses nbt

--- src/main/java/dev/willyelton/crystal_tools/crafting/CrystalElytraRecipe.java
+++ src/main/java/dev/willyelton/crystal_tools/crafting/CrystalElytraRecipe.java
@@ -1,24 +1,90 @@
 package dev.willyelton.crystal_tools.crafting;
 
+import dev.willyelton.crystal_tools.config.CrystalToolsConfig;
 import dev.willyelton.crystal_tools.item.armor.ModArmor;
+import dev.willyelton.crystal_tools.utils.NBTUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class CrystalElytraRecipe extends CustomRecipe {
+
     public CrystalElytraRecipe(ResourceLocation pId) {
         super(pId);
     }
 
     @Override
     public boolean matches(@NotNull CraftingContainer container, @NotNull Level level) {
+        List<ItemStack> items = this.getItems(container);
+
+        ItemStack elytraItem = items.get(0);
+        ItemStack crystalChestPlateItem = items.get(1);
+
+        return !elytraItem.isEmpty() && !crystalChestPlateItem.isEmpty();
+    }
+
+    @Override
+    public @NotNull ItemStack assemble(@NotNull CraftingContainer container) {
+        ItemStack stack = new ItemStack(ModArmor.CRYSTAL_ELYTRA.get());
+        List<ItemStack> items = this.getItems(container);
+
+        ItemStack elytraItem = items.get(0);
+        ItemStack crystalChestPlateItem = items.get(1);
+
+        if (crystalChestPlateItem.isEmpty()) {
+            return crystalChestPlateItem;
+        }
+
+        int[] points = NBTUtils.getIntArray(crystalChestPlateItem, "points");
+
+        // points in chestplate
+        int spentPoints = Arrays.stream(points).sum();
+        // unspent points in chestplate
+        int unspentPoints = (int) NBTUtils.getFloatOrAddKey(crystalChestPlateItem, "skill_points");
+
+        // points from enchantments on elytra
+        Map<Enchantment, Integer> enchantments = EnchantmentHelper.getEnchantments(elytraItem);
+        int enchantmentPoints = enchantments.values().stream().mapToInt(Integer::intValue).sum();
+
+        int totalPoints = spentPoints + unspentPoints + enchantmentPoints;
+
+        // Set skill points
+        NBTUtils.setValue(stack, "skill_points", totalPoints);
+
+        // Set exp cap
+        NBTUtils.setValue(stack, "experience_cap", (float) (CrystalToolsConfig.BASE_EXPERIENCE_CAP.get() * Math.pow(CrystalToolsConfig.EXPERIENCE_MULTIPLIER.get(), totalPoints)));
+
+        // Set exp
+        NBTUtils.setValue(stack, "experience", NBTUtils.getFloatOrAddKey(crystalChestPlateItem, "experience"));
+
+        return stack;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int width, int height) {
+        return width * height >= 2;
+    }
+
+    @Override
+    public @NotNull RecipeSerializer<?> getSerializer() {
+        return ModRecipes.CRYSTAL_ELYTRA_RECIPE.get();
+    }
+
+    private List<ItemStack> getItems(CraftingContainer container) {
         ItemStack elytraItem = ItemStack.EMPTY;
         ItemStack crystalChestPlateItem = ItemStack.EMPTY;
+
 
         for (int i = 0; i < container.getContainerSize(); i++) {
             ItemStack itemStack = container.getItem(i);
@@ -30,21 +96,6 @@ public class CrystalElytraRecipe extends CustomRecipe {
             }
         }
 
-        return !elytraItem.isEmpty() && !crystalChestPlateItem.isEmpty();
-    }
-
-    @Override
-    public @NotNull ItemStack assemble(@NotNull CraftingContainer container) {
-        return new ItemStack(ModArmor.CRYSTAL_ELYTRA.get());
-    }
-
-    @Override
-    public boolean canCraftInDimensions(int width, int height) {
-        return width * height >= 2;
-    }
-
-    @Override
-    public @NotNull RecipeSerializer<?> getSerializer() {
-        return ModRecipes.CRYSTAL_ELYTRA_RECIPE.get();
+        return List.of(elytraItem, crystalChestPlateItem);
     }
 }
