59c93869e07a3840dd29c47cb95ebe6e7e368930
Will Yelton <willhyelton@gmail.com>
2022-10-11 14:10:04 -0400

    Removes unneeded parameters for addExp

--- src/main/java/dev/willyelton/crystal_tools/levelable/block/entity/LevelableBlockEntity.java
+++ src/main/java/dev/willyelton/crystal_tools/levelable/block/entity/LevelableBlockEntity.java
@@ -23,22 +23,17 @@ public abstract class LevelableBlockEntity extends BlockEntity {
 
     public abstract String getBlockType();
 
-    public void addExp(Level level, BlockPos blockPos, LivingEntity player) {
-        addExp(level, blockPos, player, 1);
+    public void addExp() {
+        addExp(1);
     }
 
-    public void addExp(Level level, BlockPos blockPos, LivingEntity livingEntity, int amount) {
+    public void addExp(int amount) {
         int newExperience = (int) NBTUtils.addValueToTag(this.getPersistentData(), "experience", amount);
         int experienceCap = (int) NBTUtils.getFloatOrAddKey(this.getPersistentData(), "experience_cap", CrystalToolsConfig.BASE_EXPERIENCE_CAP.get());
 
         if (newExperience >= experienceCap) {
             // level up
             NBTUtils.addValueToTag(this.getPersistentData(), "skill_points", 1);
-            // play level up sound
-            level.playSound(null, blockPos.getX(), blockPos.getY(), blockPos.getZ(), SoundEvents.PLAYER_LEVELUP, SoundSource.NEUTRAL, 0.8F, 1.0F);
-            if (livingEntity instanceof Player player) {
-                player.displayClientMessage(Component.literal(this.getBlockType() + " Leveled Up (" + this.getSkillPoints() + " Unspent Points)"), true);
-            }
             NBTUtils.setValue(this.getPersistentData(), "experience", Math.max(0, newExperience - experienceCap));
             ToolUtils.increaseExpCap(this.getPersistentData());
         }
