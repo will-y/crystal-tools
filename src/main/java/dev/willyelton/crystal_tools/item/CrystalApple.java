0c5c378c8545bf9f19b8f5759fc2bca1b8cfc785
Will Yelton <willhyelton@gmail.com>
2022-10-24 19:08:50 -0400

    Apple now works as food

--- src/main/java/dev/willyelton/crystal_tools/item/CrystalApple.java
+++ src/main/java/dev/willyelton/crystal_tools/item/CrystalApple.java
@@ -1,16 +1,16 @@
 package dev.willyelton.crystal_tools.item;
 
+import dev.willyelton.crystal_tools.CreativeTabs;
 import dev.willyelton.crystal_tools.item.tool.LevelableTool;
 import dev.willyelton.crystal_tools.utils.NBTUtils;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,8 +19,8 @@ public class CrystalApple extends LevelableTool {
     private static final float BASE_SATURATION = 0.4F;
     private static final int BASE_EAT_SPEED = 32;
 
-    public CrystalApple(Properties properties, TagKey<Block> mineableBlocks, String itemType, int durability) {
-        super(properties, mineableBlocks, itemType, durability);
+    public CrystalApple() {
+        super(new Item.Properties().fireResistant().tab(CreativeTabs.CRYSTAL_TOOLS_TAB), null, "apple", 50);
     }
 
     public FoodProperties getFoodProperties(ItemStack stack, @Nullable LivingEntity entity) {
@@ -49,7 +49,7 @@ public class CrystalApple extends LevelableTool {
         if (itemstack.getItem() instanceof CrystalApple) {
             if (player.canEat(itemstack.getFoodProperties(player).canAlwaysEat())) {
                 player.startUsingItem(usedHand);
-                return InteractionResultHolder.consume(itemstack);
+                return InteractionResultHolder.pass(itemstack);
             } else {
                 return InteractionResultHolder.fail(itemstack);
             }
@@ -62,5 +62,17 @@ public class CrystalApple extends LevelableTool {
     public int getUseDuration(@NotNull ItemStack stack) {
         int eatSpeed = BASE_EAT_SPEED - (int) NBTUtils.getFloatOrAddKey(stack, "eat_speed_bonus");
         return Math.max(eatSpeed, 4);
+    }
+
+    @Override
+    public boolean isEdible() {
+        return true;
+    }
+
+    @Override
+    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, @NotNull Level level, @NotNull LivingEntity livingEntity) {
+        livingEntity.eat(level, stack);
+        stack.grow(1);
+        return stack;
     }
 }
