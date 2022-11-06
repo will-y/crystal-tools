dfab9cace8d27b8490e6b515033b5d29f7769a56
Will Yelton <willhyelton@gmail.com>
2022-11-05 22:30:51 -0400

    Adds animation to the bottom fuel bar with temp frames

--- src/main/java/dev/willyelton/crystal_tools/gui/CrystalFurnaceScreen.java
+++ src/main/java/dev/willyelton/crystal_tools/gui/CrystalFurnaceScreen.java
@@ -45,10 +45,23 @@ public class CrystalFurnaceScreen extends AbstractContainerScreen<CrystalFurnace
     private static final int UPGRADE_BUTTON_WIDTH = 12;
     private static final int UPGRADE_BUTTON_HEIGHT = 12;
 
+    private static final int FUEL_BAR_X = 28;
+    private static final int FUEL_BAR_Y = 91;
+    private static final int FUEL_BAR_TEXTURE_X = 0;
+    private static final int FUEL_BAR_TEXTURE_Y = 191;
+    private static final int FUEL_BAR_INITIAL_WIDTH = 44;
+    private static final int FUEL_BAR_HEIGHT = 2;
+    private static final int FUEL_BAR_WIDTH_INCREASE = 20;
+
     private final NonNullList<Slot> slots;
     private final CrystalFurnaceBlockEntity blockEntity;
     private FurnaceUpgradeButton upgradeButton;
     private final float expLabelX;
+
+    private int counter = 0;
+    private int maxCounter = 10;
+    private int animFrame = 0;
+    private int maxAnimFrame = 3;
 
     public CrystalFurnaceScreen(CrystalFurnaceContainer container, Inventory inventory, Component name) {
         super(container, inventory, name);
@@ -68,6 +81,7 @@ public class CrystalFurnaceScreen extends AbstractContainerScreen<CrystalFurnace
         int j = this.topPos;
         this.blit(poseStack, i, j, 0, 0, this.imageWidth, this.imageHeight);
         this.renderSlots(poseStack);
+        this.renderFuelBar(poseStack);
     }
 
     @Override
@@ -90,18 +104,35 @@ public class CrystalFurnaceScreen extends AbstractContainerScreen<CrystalFurnace
                     this.blit(poseStack, slot.x + 3 + this.leftPos, slot.y + SLOT_TEXTURE_SIZE + this.topPos + ARROW_HEIGHT - height, ARROW_TEXTURE_ON_X, ARROW_TEXTURE_Y + ARROW_HEIGHT - height, ARROW_WIDTH, height);
                 } else if (slot instanceof CrystalFurnaceInputSlot) {
                     // Draw fire below
-                    this.blit(poseStack, slot.x + 1 + this.leftPos, slot.y + SLOT_TEXTURE_SIZE + this.topPos, FIRE_TEXTURE_X, FIRE_TEXTURE_Y, FIRE_TEXTURE_WIDTH, FIRE_TEXTURE_HEIGHT);
+                    this.blit(poseStack, slot.x + 1 + this.leftPos, slot.y + SLOT_TEXTURE_SIZE + this.topPos + 2, FIRE_TEXTURE_X, FIRE_TEXTURE_Y, FIRE_TEXTURE_WIDTH, FIRE_TEXTURE_HEIGHT);
                     // Draw lit progress
                     if (this.menu.isLit()) {
                         float litProgress = this.menu.getLitProgress();
                         int height = (int) (litProgress * 13);
-                        this.blit(poseStack, slot.x + 2 + this.leftPos, slot.y + SLOT_TEXTURE_SIZE + this.topPos + FIRE_TEXTURE_HEIGHT - height, FIRE_TEXTURE_ON_X, FIRE_TEXTURE_Y + FIRE_TEXTURE_HEIGHT - height, FIRE_TEXTURE_WIDTH, height);
+                        this.blit(poseStack, slot.x + 2 + this.leftPos, slot.y + SLOT_TEXTURE_SIZE + this.topPos + FIRE_TEXTURE_HEIGHT - height + 2, FIRE_TEXTURE_ON_X, FIRE_TEXTURE_Y + FIRE_TEXTURE_HEIGHT - height, FIRE_TEXTURE_WIDTH, height);
                     }
                 } else if (slot instanceof CrystalFurnaceFuelSlot && slot.index != 10) {
                     // Draw fuel arrow thing
                     this.blit(poseStack, slot.x + 4 + this.leftPos, slot.y + 19 + this.topPos, FUEL_ARROW_TEXTURE_X, FUEL_ARROW_TEXTURE_Y, FUEL_ARROW_TEXTURE_WIDTH, FUEL_ARROW_TEXTURE_HEIGHT);
                 }
             }
+        }
+    }
+
+    private void renderFuelBar(PoseStack poseStack) {
+        if (counter > maxCounter) {
+            counter = 0;
+            this.animFrame += 1;
+            if (this.animFrame > this.maxAnimFrame) this.animFrame = 0;
+        }
+        counter++;
+
+        int widthIncrease = (this.menu.getNumActiveSlots() - 1) * FUEL_BAR_WIDTH_INCREASE;
+        this.blit(poseStack, this.leftPos + FUEL_BAR_X, this.topPos + FUEL_BAR_Y, FUEL_BAR_TEXTURE_X, FUEL_BAR_TEXTURE_Y, FUEL_BAR_INITIAL_WIDTH + widthIncrease, FUEL_BAR_HEIGHT);
+
+        if (this.menu.isLit()) {
+            this.blit(poseStack, this.leftPos + FUEL_BAR_X, this.topPos + FUEL_BAR_Y, FUEL_BAR_TEXTURE_X, FUEL_BAR_TEXTURE_Y +( this.animFrame + 1) * 2, FUEL_BAR_INITIAL_WIDTH + widthIncrease, FUEL_BAR_HEIGHT);
+
         }
     }
 
